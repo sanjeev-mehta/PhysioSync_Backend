e697e4c7e5b4213436c67223f2b8e57bd3dd450a
rohxoxo <rohitnicbc@gmail.com>
2024-06-24 21:52:30 -0700

    ci: edit exercise updated api
    This commit includes the updated edit exercise.

--- src/controllers/exercise/exercises_controller.ts
+++ src/controllers/exercise/exercises_controller.ts
@@ -68,18 +68,35 @@ export const getAllExercise = async (req: Request, res: Response): Promise<void>
 
 export const updateExercise = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { exerciseId } = req.params;
-    const updateData = req.body;
-    
+    const { exerciseId, sessionToken } = req.params;
+    const { category_id, category_name, video_Url, video_title, description, video_thumbnail } = req.body;
+
+    const therapist = await Therapist.findOne({ sessionToken: sessionToken });
+
+    if (!therapist) {
+      res.status(404).json({ success: false, message: 'Therapist not found, please login again' });
+      return;
+    }
+
+    const updateData = {
+      therapist_id: therapist._id,
+      category_id,
+      category_name,
+      video_Url,
+      video_title,
+      description,
+      video_thumbnail
+    };
+
     const updatedExercise = await addExerciseModel.findByIdAndUpdate(exerciseId, updateData, { new: true });
-    
+
     if (!updatedExercise) {
       res.status(404).json({ message: 'Exercise not found', success: false });
       return;
     }
 
-    res.status(200).json({message: 'Exercise has been Updated', success: true});
-  
+    res.status(200).json({ message: 'Exercise has been updated', success: true, data: updatedExercise });
+
   } catch (error) {
     console.error('Error updating exercise:', error);
     res.status(500).json({ message: 'Internal Server Error', success: false });
