da9e9ddde32a45891bf065ec2b6cad4d449af53b
rohxoxo <rohitnicbc@gmail.com>
2024-06-24 20:18:22 -0700

    Update exercises_model.ts

--- src/models/exercise/exercises_model.ts
+++ src/models/exercise/exercises_model.ts
@@ -3,7 +3,7 @@ import mongoose, { Document, Schema, Types } from 'mongoose';
 export interface AddExercise extends Document {
     therapist_id: string;
     category_id: Types.ObjectId[];
-    category_name: string;
+    category_name: [string];
     video_Url: string;
     video_title: string;
     description: string;
@@ -13,7 +13,7 @@ export interface AddExercise extends Document {
 const addExerciseSchema = new Schema({
     therapist_id: { type: String, required: true },
     category_id: [{ type: Schema.Types.ObjectId, ref: 'Category', required: true }],
-    category_name: { type: String, required: true },
+    category_name: [{ type: String, required: true }],
     video_Url: { type: String, required: true },
     video_title: { type: String, required: true },
     description: { type: String, required: true },
