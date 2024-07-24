d7a5be7a00d44ab5e108a57b298493c0810ab998
rohxoxo <rohitnicbc@gmail.com>
2024-07-24 12:05:05 -0700

    Update therapistController.ts

--- src/controllers/Therapist/therapistController.ts
+++ src/controllers/Therapist/therapistController.ts
@@ -39,7 +39,7 @@ export const addTherapist = async (req: Request, res: Response) => {
 export const editTherapistController = async (req: Request, res: Response) => {
   try {
     const authorizationHeader = req.headers.authorization;
-    const {newData} = req.body;
+    const newData = req.body;
 
       if (!authorizationHeader) {
          res.status(401).json({ status: false, message: "Authorization header not found" });
@@ -47,6 +47,7 @@ export const editTherapistController = async (req: Request, res: Response) => {
       }
 
       const [bearer, sessionToken] = authorizationHeader.split(' ');
+      console.log("This is my session",sessionToken);
 
     if (!sessionToken || bearer !== 'Bearer') {
        res.status(401).json({ status: false, message: "Session token not found or invalid format" });
